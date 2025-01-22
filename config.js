7414d270245fa0171418067a1de26ef9567ced1c
Mizzuu-uuzziM <fhmieynjb123@gmail.com>
2025-01-22 15:32:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
-/*=============================================
-VERSION UPDATE
-UPDATE TERAKHIR 15 November 2024 JAM 00:00 WIB
-GITHUB : TanakaDomp
+// =============================================
+// VERSION UPDATE
+// UPDATE TERAKHIR 15 November 2024 JAM 00:00 WIB
+// GITHUB : TanakaDomp
+// VERSION : 1.1.0
+// =============================================
 
-VERSION : 1.1.0
-=============================================
-*/
 const fs = require('fs')
 const chalk = require('chalk')
 const moment = require('moment-timezone')
@@ -13,16 +12,17 @@ const hariini = moment.tz('Asia/Jakarta').format('dddd, DD MMMM YYYY')
 const time = moment(Date.now()).tz('Asia/Jakarta').locale('id').format('HH:mm:ss z')
 
 //====== [ SETTING PAIRING & TOKEN ] ========//
-global.pairing = '6289633188822'//isi dengan format 6281xxxx..., agar mendapatkan kode pairing
+global.pairing = '6289633188822'
 //====== [ SETTING OWNER  ] ========//
-global.owner = [ '-', '-' ]//isi dengan format 6281xxxx...
-global.owner0 = '-@s.whatsapp.net'//isi dengan format 6281xxxx@s.whatsapp.net
-global.ownernumber = '6281359932022'//isi dengan format 6281xxxx...
+global.owner = [ '6281359932022', '6288807709243','6281399897454','6283841951316' ]
+global.dontTagOwn = '6281359932022'
+global.owner0 = ['6281359932022@s.whatsapp.net','6288807709243@s.whatsapp.net','6281399897454@s.whatsapp.net','6283841951316@s.whatsapp.net']
+global.ownernumber = ['6281359932022','6288807709243','6281399897454','6283841951316']
 global.ownername = 'ㅆíㄹㄹùù'
 //====== [ SETTING BOT ] ========//
 global.botname = 'Mizzuu • Assistant'
-global.botnumber = ''//isi dengan format 6281xxxx...
-global.bot_version = 'V 0.0.2 (_CPANEL VERSION_)'
+global.botnumber = '6289633188822'
+global.bot_version = 'V 0.0.1 (_CPANEL VERSION_)'
 global.prefa = ['/','!','.','#','&']
 //====== [ SETTING PACKNAME ] ========//
 global.packname = 'ׄ 𓄯ִ ── ꯭𐑈ƚꪱִ𝖼𝗄ᧉׄ𝗋 ᎓  lylꪱc𝗁α𝗇 ხᦢƚ x Mizzuu '
@@ -30,28 +30,36 @@ global.author = `Crᧉׄdı๋𝗍 : 森晓伟 ταიακαdοოρ.ხiz.id\nA
 global.themeemoji = '🍁'
 global.wm = " TanakaSense.js."
 global.idch = '--@newsletter'
+global.github = 'https://github.com/Mizzuu-uuzziM'
 global.titlech = '--'
 //====== [ THEME URL & URL ] ========//
-global.thumbnail = 'https://files.catbox.moe/p4g53y.jpg'//bebas bisa kalian ganti 
+global.thumbnail = 'https://files.catbox.moe/n60pjq.jpg'
 global.Url = 'https://whatsapp.com/channel/0029VaW25g5F1YlKczMRmd1h'
+
+//====== [ GLOBAL PANEL ] ======//
+global.domain = 'https://ditashopxrifky.panelku-vip.my.id' // isi dengan domain panel lu
+global.apikey = 'ptla_VhFEumG6cfz7SbxpXVd3tv3s2uvTrefln1ydxvwKwJ5' // Isi Apikey Plta Lu
+global.capikey = 'ptlc_0Zk9dSAJKe9GjMpsyA1Cja9VV9iq40JlhNtWBIwOEsY' // Isi Apikey Pltc Lu
+global.eggsnya = '15' // id eggs yang dipakai
+global.location = '1' // id location
+
 //====== [ GLOBAL MESSAGES  ] ========//
 global.mess = {
-    done: '*( 🎂 )* Success!!',
-    prem: 'Sorry, this feature can only be used by premium users.\nIf you want to buy premium, type *.buyprem*',
+    done: '*( 🎂 )* ```Success...My Favorite Owner```',
+    prem: '`Sorry, this feature can only be used by premium users.\nIf you want to buy premium, type *.buyprem*`',
     admin: '*( Error )* Fitur ini khusus untuk penjabat!!',
-    botadmin: '*( Error )* Sepertinya lilychanj bukan admin!!',
-    owner: '*( Error )* Kamu bukan ownerkuu!!',
+    botadmin: '*( Error )* Sepertinya... Aku bukan admin🤔!!',
+    owner: '*( Error )* Kamu bukan ownerkuu>_<!!',
     group: '*( Error )* Fitur ini khusus dalam group!!',
     private: '*( Error )* Fitur ini hanya untuk private chat!!',
-    wait: '*( Loading )* Chotto matte...',
+    wait: '*( Loading )* Chotto matte... Kyaahhh....><',
     error: 'Error!',
 }
 /*isi list kalian disini, jangan gunakan tanda petik ``, karna bakal eror kalau kalian pakai itu*/
 global.list = `list produk? isi sendiri`
 //  - AKHIR -
 //  JIKA ADA YANG KURANG MENGERTI SILAHKAN CHAT ADMIN DI WHATSAPP MAUPUN DI TELEGRAM OKEY
-//  - Number Tanaka : +62 815 4117 7589
-//  - Number Mizzuu : +62 813-5993-2022
+//  - Number : +62 815 4117 7589
 //  - Telegram : @tanaka_senn
 
 
